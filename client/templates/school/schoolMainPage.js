532f4af4cdaab27c06325431db1247a83891c7e6
Robospace <ruslan@katev.kz>
2017-08-27 10:20:56 +0600

    fixed index rouute

--- client/templates/school/schoolMainPage.js
+++ client/templates/school/schoolMainPage.js
@@ -3,25 +3,13 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './schoolMainPage.html';
 Template.schoolMainPage.onCreated(function() {
     let template = this
+    template.subscribe("subjects")
 })
 Template.schoolMainPage.helpers({
     subjects() {
         return Subjects.find({},{sort:{subjectId:1}})
-    },
-    schools() {
-        return Schools.find({},{sort:{schoolId:1}})
     }
 });
 
 Template.schoolMainPage.events({
-    'click #resetpassword'() {
-        if (confirm("Are u sure?"))
-            Meteor.call('resetSchoolPassword',this.schoolId,(err,res) => {
-            if (err) {
-                alert(err.reason)
-            } else {
-                alert('Сақталды')
-            }
-        })
-    }
 })
